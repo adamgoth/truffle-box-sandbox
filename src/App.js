77140dc0ef416685ad0eae559e0bde451f6c46b6
Adam Goth <adamgoth@gmail.com>
2017-05-10 21:25:28 -0600

    Adds button for buyTicket function

--- src/App.js
+++ src/App.js
@@ -19,65 +19,46 @@ class App extends Component {
   }
 
   componentWillMount() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    // So we can update state later.
     var self = this
-
-    // Get the RPC provider and setup our SimpleStorage contract.
     const provider = new Web3.providers.HttpProvider('http://localhost:8545')
     const contract = require('truffle-contract')
-    //const simpleStorage = contract(SimpleStorageContract)
     const conference = contract(Conference)
-    //simpleStorage.setProvider(provider)
     conference.setProvider(provider)
-
-    // Get accounts.
-    // web3RPC.eth.getAccounts(function(error, accounts) {
-    //   console.log(accounts)
-    //
-    //    simpleStorage.deployed().then(function(instance) {
-    //      simpleStorageInstance = instance
-    //
-    //
-    //     // Stores a value of 5.
-    //     return simpleStorageInstance.set(5, {from: accounts[0]})
-    //   }).then(function(result) {
-    //     // Get the value from the contract to prove it worked.
-    //     return simpleStorageInstance.get.call(accounts[0])
-    //   }).then(function(result) {
-    //     // Update state with the result.
-    //     return self.setState({ storageValue: result.c[0] })
-    //   })
-    // })
-
-    // Get Web3 so we can get our accounts.
     const web3RPC = new Web3(provider)
-
     var conferenceInstance
-
     web3RPC.eth.getAccounts(function(error, accounts) {
       console.log(accounts)
-
       conference.deployed().then(function(instance) {
         conferenceInstance = instance
-
         return conferenceInstance.numRegistrants.call()
       }).then(function(result) {
-
         return self.setState({ accounts, numRegistrants: result.c[0] })
+      })
+    })
+  }
+
+  buyTicket = () => {
+    var self = this
+    const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+    const contract = require('truffle-contract')
+    const conference = contract(Conference)
+    conference.setProvider(provider)
+    var conferenceInstance
+    conference.deployed().then(function(instance) {
+      conferenceInstance = instance
+      return conferenceInstance.buyTicket({ from: '0x7d8f8038bf1a353c5a7eb3ba43120081e665d7b6' , value: 100 })
+    }).then(
+      function() {
+        return conferenceInstance.numRegistrants.call()
+      }).then(function(result) {
+        return self.setState({ numRegistrants: result.c[0]
       })
     })
   }
 
   render() {
     console.log(this.state)
-    
+
     var displayAccounts = this.state.accounts.map(account => {
       return <p key={account}>{account}</p>;
     });
@@ -98,6 +79,8 @@ class App extends Component {
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
+              <h2>Something</h2>
+              <button onClick={this.buyTicket}>Buy Ticket</button>
               <h2>Number of registrants</h2>
               <p>The number of registrants is: {this.state.numRegistrants}</p>
               <h2>Accounts</h2>
